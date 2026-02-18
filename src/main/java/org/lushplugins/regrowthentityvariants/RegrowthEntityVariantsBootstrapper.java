34cde9e75c3173d42650a7c6dc17e37da381cf4e
OakLoaf <oak@beaconstudios.org>
2026-02-18 22:38:08 +0000

    Made cow, pig and chicken use basic model for variants

--- src/main/java/org/lushplugins/regrowthentityvariants/RegrowthEntityVariantsBootstrapper.java
+++ src/main/java/org/lushplugins/regrowthentityvariants/RegrowthEntityVariantsBootstrapper.java
@@ -2,6 +2,9 @@ package org.lushplugins.regrowthentityvariants;
 
 import io.papermc.paper.plugin.bootstrap.BootstrapContext;
 import io.papermc.paper.plugin.bootstrap.PluginBootstrap;
+import io.papermc.paper.registry.data.ChickenVariantRegistryEntry;
+import io.papermc.paper.registry.data.CowVariantRegistryEntry;
+import io.papermc.paper.registry.data.PigVariantRegistryEntry;
 import io.papermc.paper.registry.event.RegistryEvents;
 import io.papermc.paper.registry.keys.*;
 import org.jspecify.annotations.NonNull;
@@ -17,8 +20,11 @@ public final class RegrowthEntityVariantsBootstrapper implements PluginBootstrap
 
         constructor.simpleVariants(RegistryEvents.CAT_VARIANT.compose(), "cat", CatVariantKeys::create, texture -> b -> b.clientTextureAsset(texture));
         constructor.simpleVariants(RegistryEvents.FROG_VARIANT.compose(), "frog", FrogVariantKeys::create, texture -> b -> b.clientTextureAsset(texture));
-        constructor.simpleVariants(RegistryEvents.CHICKEN_VARIANT.compose(), "chicken", ChickenVariantKeys::create, texture -> b -> b.clientTextureAsset(texture));
-        constructor.simpleVariants(RegistryEvents.COW_VARIANT.compose(), "cow", CowVariantKeys::create, texture -> b -> b.clientTextureAsset(texture));
-        constructor.simpleVariants(RegistryEvents.PIG_VARIANT.compose(), "pig", PigVariantKeys::create, texture -> b -> b.clientTextureAsset(texture));
+        constructor.simpleVariants(RegistryEvents.CHICKEN_VARIANT.compose(), "chicken", ChickenVariantKeys::create, texture -> b -> b.clientTextureAsset(texture)
+            .model(ChickenVariantRegistryEntry.Model.NORMAL));
+        constructor.simpleVariants(RegistryEvents.COW_VARIANT.compose(), "cow", CowVariantKeys::create, texture -> b -> b.clientTextureAsset(texture)
+            .model(CowVariantRegistryEntry.Model.NORMAL));
+        constructor.simpleVariants(RegistryEvents.PIG_VARIANT.compose(), "pig", PigVariantKeys::create, texture -> b -> b.clientTextureAsset(texture)
+            .model(PigVariantRegistryEntry.Model.NORMAL));
     }
 }
